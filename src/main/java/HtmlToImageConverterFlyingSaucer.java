cd952d666a850763262da4f75a79b37712eb7fc9
DungNt <tandung.nguyen@migitek.com>
2025-04-15 15:47:00 +0700

    render pdf Le Van Anh

--- src/main/java/HtmlToImageConverterFlyingSaucer.java
+++ src/main/java/HtmlToImageConverterFlyingSaucer.java
@@ -1,4 +1,11 @@
 import com.example.testimage.dto.CertificateGenerateDTO;
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
@@ -12,6 +19,7 @@ import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -29,7 +37,7 @@ public class HtmlToImageConverterFlyingSaucer {
     public static void main(String[] args) throws IOException, FontFormatException {
         CertificateGenerateDTO certificateGenerateDTO = new CertificateGenerateDTO();
         certificateGenerateDTO.setLogoSellerUrl("https://cdn-migi-2.laosedu.la/f/laosedu/4fc25ce6eb8c97f4cb1deb10be041ce3/1f0a2d5c2b9e171c8da66644086577cefa0f60b414784c8de2e31006ec3a2ffb/taoanhdep_chu_ky_85765.png");
-        certificateGenerateDTO.setStudentName("Nguyễn Ngọc Hoàngພາສາລາວມີຕົວອັກສອນຫຼາຍແບບ");
+        certificateGenerateDTO.setStudentName("Nguyễn Ngọc Hoàngພາສາລາວມີຕົວອັຫຼາຍແບບ \uD83C\uDF89\uD83C\uDF89");
         certificateGenerateDTO.setCourseName("Lập trình Java Lập trình Java Lập trình JavaLập trình JavaLập trình Java Lập trình Java Lập trình Java Lập trình Java");
         certificateGenerateDTO.setCertificateTime(Instant.now());
         // Chạy hàm chuyển đổi HTML sang hình ảnh
@@ -92,13 +100,6 @@ public class HtmlToImageConverterFlyingSaucer {
             File tempHtmlFile = File.createTempFile("certificate-", ".html");
             Files.write(tempHtmlFile.toPath(), processedHtml.getBytes(StandardCharsets.UTF_8));
 
-            // 6. Render HTML thành hình ảnh
-            int width = 842;
-            BufferedImage image = Graphics2DRenderer.renderToImageAutoSize(
-                    tempHtmlFile.toURI().toURL().toExternalForm(),
-                    width,
-                    BufferedImage.TYPE_INT_ARGB
-            );
             File dir = new File(outputDirectory);
             File[] oldImages = dir.listFiles((d, name) -> name.startsWith("certificate-") && name.endsWith(".png"));
             if (oldImages != null) {
@@ -110,14 +111,35 @@ public class HtmlToImageConverterFlyingSaucer {
                     }
                 }
             }
-
-            // 7. Lưu hình ảnh đầu ra
+//
             String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
             String randomId = UUID.randomUUID().toString().substring(0, 8);
-            String outputPath = outputDirectory + "certificate-" + timestamp + "-" + randomId + ".png";
-            ImageIO.write(image, "png", new File(outputPath));
-            System.out.println("Image created successfully: " + outputPath);
 
+            String pdfOutputPath = outputDirectory + "certificate-" + timestamp + "-" + randomId + ".pdf";
+
+            ConverterProperties properties = new ConverterProperties();
+            PdfWriter writer = new PdfWriter(pdfOutputPath);
+            PdfDocument pdfDoc = new PdfDocument(writer);
+
+            pdfDoc.setDefaultPageSize(new PageSize(842, 600));
+
+            HtmlConverter.convertToPdf(processedHtml, pdfDoc, properties);
+            pdfDoc.close();
+
+            System.out.println("PDF created successfully: " + pdfOutputPath);
+
+            PDDocument document = PDDocument.load(new File(pdfOutputPath));
+            PDFRenderer pdfRenderer = new PDFRenderer(document);
+
+            int dpi = 600;
+            BufferedImage bim = pdfRenderer.renderImageWithDPI(0, dpi);
+
+            String imageOutputPath = outputDirectory + "certificate-" + timestamp + "-" + randomId + ".png";
+
+            ImageIO.write(bim, "png", new File(imageOutputPath));
+            document.close();
+
+            System.out.println("Image created successfully: " + imageOutputPath);
             boolean deleted = tempHtmlFile.delete();
             if (!deleted) {
                 System.err.println("Cảnh báo: Không thể xóa file tạm " + tempHtmlFile.getPath());
